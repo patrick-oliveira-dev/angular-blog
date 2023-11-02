7bcb85b42e7196c1cad8d1fa10cae15ddb6aa3e0
Patrick Oliveira <patrick17silva@hotmail.com>
2023-11-02 20:46:22 -0300

    Aplicando inteligencia nos componentes com binding

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SmallCardComponent implements OnInit {
 
+  @Input() photoCover:string = ""
+  @Input() publishedDate:string = ""
+  @Input() cardTitle:string = ""
+
   constructor() { }
 
   ngOnInit(): void {
+
+
   }
 
 }
